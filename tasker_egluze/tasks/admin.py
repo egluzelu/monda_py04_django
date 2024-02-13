b434e5de74700a36f7353fcb37c3dcca1d20e79f
egluzelu <egluze.lu@gmail.com>
2024-02-13 10:10:35 +0200

    Django 2 diena, neužbaigta

--- tasker_egluze/tasks/admin.py
+++ tasker_egluze/tasks/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.utils import
 from . import models
-admin.site.register(models.Project)
-admin.site.register(models.Task)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'owner']
+    list_display_links = ['id', 'name']
+    list_filter = ['owner']
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_done', 'deadline', 'project' 'owner', 'created_at']
+    list_filter = ['is done', 'deadline', 'created_at']
+    search_fields = ['name', 'description', 'project_name', 'owner_last_name']
+
+
+
+admin.site.register(models.Project, ProjectAdmin)
+admin.site.register(models.Task, TaskAdmin)
 
 # Register your models here.
